4c75284702ef4c4eb1b606316c2d252ee597676f
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-10-18 10:18:19 -0400

    Built a function that triggers inquirer and wrote questions. Questions asked depend on the role of employee. Initialized the function.

--- app.js
+++ app.js
@@ -10,9 +10,171 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Define an array that holds all employee objects.
+let employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Define a list of questions to ask if the employee is a manager.
+const managerQuestions = [
+  {
+    type: "input",
+    message: "What is the manager's name?",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "What is the manager's ID number?",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "What is the manager's email address?",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "What is the manager's office phone number?",
+    name: "managerOffice",
+  },
+  {
+    type: "confirm",
+    message: "Add another employee?",
+    name: "repeatInquirer",
+    default: false,
+  },
+];
+
+// Define a list of questions to ask if the employee is an engineer.
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "What is the engineer's name?",
+    name: "engineerName",
+  },
+  {
+    type: "input",
+    message: "What is the engineer's ID number?",
+    name: "engineerId",
+  },
+  {
+    type: "input",
+    message: "What is the engineer's email address?",
+    name: "engineerEmail",
+  },
+  {
+    type: "input",
+    message: "What is the engineer's GitHub username?",
+    name: "engineerGithub",
+  },
+  {
+    type: "confirm",
+    message: "Add another employee?",
+    name: "repeatInquirer",
+    default: false,
+  },
+];
+
+// Define a list of questions to ask if the employee is an intern.
+const internQuestions = [
+  {
+    type: "input",
+    message: "What is the intern's name?",
+    name: "internName",
+  },
+  {
+    type: "input",
+    message: "What is the intern's ID number?",
+    name: "internId",
+  },
+  {
+    type: "input",
+    message: "What is the intern's email address?",
+    name: "internEmail",
+  },
+  {
+    type: "input",
+    message: "Which school did the intern go to?",
+    name: "internSchool",
+  },
+  {
+    type: "confirm",
+    message: "Add another employee?",
+    name: "repeatInquirer",
+    default: false,
+  },
+];
+
+// Define a function that initializes the data collection.
+// Change which questions are asked based on the employee's role.
+let init = (cb1, cb2) => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What is the employee's role?",
+        name: "employeeRole",
+        choices: ["Manager", "Engineer", "Intern"],
+        // Validate that only one manager exists per team.
+      },
+    ])
+    .then((response) => {
+      switch (response.employeeRole) {
+        case "Manager":
+          inquirer.prompt(managerQuestions).then((response) => {
+            const manager = new Manager(
+              response.managerName,
+              response.managerId,
+              response.managerEmail,
+              response.managerOffice
+            );
+            employees.push(manager);
+            if (response.repeatInquirer) {
+              init(cb1);
+            } else {
+              console.log(employees);
+              let result = cb1(employees);
+            }
+          });
+          break;
+        case "Engineer":
+          inquirer.prompt(engineerQuestions).then((response) => {
+            const engineer = new Engineer(
+              response.engineerName,
+              response.engineerId,
+              response.engineerEmail,
+              response.engineerGithub
+            );
+            employees.push(engineer);
+            if (response.repeatInquirer) {
+              init(cb1);
+            } else {
+              console.log(employees);
+              let result = cb1(employees);
+            }
+          });
+          break;
+        case "Intern":
+          inquirer.prompt(internQuestions).then((response) => {
+            const intern = new Intern(
+              response.internName,
+              response.internId,
+              response.internEmail,
+              response.internSchool
+            );
+            employees.push(intern);
+            if (response.repeatInquirer) {
+              init(cb1);
+            } else {
+              console.log(employees);
+              let result = cb1(employees);
+            }
+          });
+          break;
+      }
+    });
+};
+
+// Initialize the data collection process.
+init(render);
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
